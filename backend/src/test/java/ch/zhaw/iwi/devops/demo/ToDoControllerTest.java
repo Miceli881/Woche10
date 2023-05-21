b5eb135960eaad6cc755a64ad82d44de53e13adc
Miceli881 <davidemiceli@hotmail.com>
2023-05-21 18:27:01 +0200

    added more tests, getter and deleter

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/ToDoControllerTest.java
@@ -1,6 +1,8 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +22,34 @@ public class ToDoControllerTest {
         ToDoController controller = new ToDoController();
         assertEquals("{ \"status\": \"ok\", \"userId\": \"admin\", \"languageCode\": \"de\",\"version\": \"0.0.1\"}", controller.ping());
     }
+
+    @Test
+    public void testGetTodo() {
+        ToDoController controller = new ToDoController();
+        var originalTodo = new ToDo(1, "important todo", "this is very important to test jenkins functionality");
+        controller.createTodo(1, originalTodo);
+        
+        var todo = controller.getTodo(1);
+        assertEquals("important todo", todo.getTitle());
+        assertEquals("this is very important to test jenkins functionality", todo.getDescription());
+    }
+
+    @Test void testCreateAndDeleteTodo() {
+        ToDoController controller = new ToDoController();
+        var originalTodo = new ToDo(1, "important todo", "this is very important to test jenkins functionality");
+        controller.createTodo(1, originalTodo);
+        
+        var todo = controller.getTodo(1);
+
+        assertNotNull(todo);
+
+        controller.deleteTodo(1);
+        
+        var todoAfter = controller.getTodo(1);
+        
+        assertNull(todoAfter);
+
+    }
+
 
 }
